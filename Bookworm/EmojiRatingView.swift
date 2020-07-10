2b19e9dce4dd5c73d63e8bffae06d391ea688342
garima94921 <gaarimabothra@gmail.com>
2020-07-10 11:55:22 +0530

    Added: RatingView and EmojiRatingView

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Garima Bothra on 10/07/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+
+    let rating: Int16
+
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("🐢")
+        case 2:
+            return Text("😞")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("😃")
+        default:
+            return Text("😍")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 4)
+    }
+}
